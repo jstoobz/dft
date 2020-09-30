c3172669434538130597f1b59b870488fa26e10f
James Stephens <stephensja7@gmail.com>
2020-09-29 21:58:15 -0400

    Update bootstrap.sh

--- script/bootstrap.sh
+++ script/bootstrap.sh
@@ -1,49 +1,48 @@
 #!/bin/sh
 
+# stops the execution of a script if a command or pipeline has an error
+set -e
+
+# Profiling - Most simply, run the script in verbose mode (in bash, use the -x flag or set -x at the top of the script) and see where execution visibly hangs – this is a very quick way to identify bottlenecks.
+# set -x 
+
 cd "$(dirname "$0")/.."
 # Prints the actual path (as opposed to symbolic)
 DOTFILES_ROOT=$(pwd -P)
 
-# stops the execution of a script if a command or pipeline has an error
-set -e
-
 # Ask for the administrator password upfront
-sudo -v
+# sudo -v
 
 # Keep-alive: update existing 'sudo' time stamp until '.osx' has finished
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+# while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-if ! xcode-select --print-path > /dev/null; then
+echo ''
 
-  # Prompt user to install the XCode Command Line Tools
-  xcode-select --install > /dev/null
+info() {
+	# shellcheck disable=SC2059
+	printf "\r  [ \033[00;34m..\033[0m ] $1\n"
+}
 
-  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+user() {
+	# shellcheck disable=SC2059
+	printf "\r  [ \033[0;33m??\033[0m ] $1\n"
+}
 
-  # Wait until the XCode Command Line Tools are installed
-  until xcode-select --print-path > /dev/null; do
-    sleep 5
-  done
+success() {
+	# shellcheck disable=SC2059
+	printf "\r\033[2K  [ \033[00;32mOK\033[0m ] $1\n"
+}
 
-  print_result $? 'Install XCode Command Line Tools'
+fail() {
+	# shellcheck disable=SC2059
+	printf "\r\033[2K  [\033[0;31mFAIL\033[0m] $1\n"
+	echo ''
+	exit
+}
 
-  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-  # Point the `xcode-select` developer directory to
-  # the appropriate directory from within `Xcode.app`
-  # https://github.com/alrra/dotfiles/issues/13
-
-  sudo xcode-select --switch /Applications/Xcode.app/Contents/Developer
-  print_result $? 'Make "xcode-select" developer directory point to Xcode'
-
-  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-  # Prompt user to agree to the terms of the Xcode license
-  # https://github.com/alrra/dotfiles/issues/10
-
-  sudo xcodebuild -license
-  print_result $? 'Agree with the XCode Command Line Tools licence'
-elif xcode-select --print-path > /dev/null; then
-    echo "XCode command line tools is already installed."
+if xcode-select -p; then
+  success "Xcode found"
+else
+  fail "Xcode not found"
 fi
-
